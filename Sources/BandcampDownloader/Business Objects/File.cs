fd80c16b8242e27d94749c8554b949223b500f9c
Otiel <web@otiel.fr>
2014-02-28 16:13:06 +0100

    Cleaned code

--- Sources/BandcampDownloader/Business Objects/File.cs
+++ Sources/BandcampDownloader/Business Objects/File.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace BandcampDownloader {
+
     internal class File {
-        public String Url { get; set; }
-        public long BytesReceived { get; set; }
-        public long Size { get; set; }
+        public String Url           { get; set; }
+        public long   BytesReceived { get; set; }
+        public long   Size          { get; set; }
 
         public File(String url, long bytesReceived, long size) {
-            this.Url = url;
+            this.Url           = url;
             this.BytesReceived = bytesReceived;
-            this.Size = size;
+            this.Size          = size;
         }
     }
 }
--- Sources/BandcampDownloader/Business Objects/Track.cs
+++ Sources/BandcampDownloader/Business Objects/Track.cs
@@ -3,8 +3,8 @@
 namespace BandcampDownloader {
 
     internal class Track {
-        public String Title { get; set; }
-        public int Number { get; set; }
+        public String Title  { get; set; }
+        public int    Number { get; set; }
         public String Mp3Url { get; set; }
 
         public String GetFileName(String artist) {
