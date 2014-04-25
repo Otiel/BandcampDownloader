f488382112c0e8cf980cea6ece56a0ea8c9f1b5b
Otiel <web@otiel.fr>
2014-04-25 16:51:11 +0200

    Updated assembly version

--- Sources/BandcampDownloader/Properties/AssemblyInfo.cs
+++ Sources/BandcampDownloader/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.4.0")]
-[assembly: AssemblyFileVersion("0.1.4.0")]
+[assembly: AssemblyVersion("0.1.4.1")]
+[assembly: AssemblyFileVersion("0.1.4.1")]
 [assembly: GuidAttribute("8C171C7F-9BAC-4EC0-A287-59908B48953F")]
