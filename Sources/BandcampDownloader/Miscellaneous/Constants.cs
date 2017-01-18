28a6faed6645ca644749323bcdb3a5eaedf2ffb7
Otiel <web@otiel.fr>
2017-01-18 10:41:00 +0100

    Updated help text
    To add track pages and whole disco

--- Sources/BandcampDownloader/Miscellaneous/Constants.cs
+++ Sources/BandcampDownloader/Miscellaneous/Constants.cs
@@ -16,7 +16,7 @@ namespace BandcampDownloader {
         /// <summary>
         /// The help text displayed in the URL list textbox.
         /// </summary>
-        public static readonly String UrlsHint = "Paste URLs of albums to download here.\nA Bandcamp URL looks like: http://[artist name].bandcamp.com/album/[album name]\nYou can specify multiple URLs by writing one URL per line.";
+        public static readonly String UrlsHint = "Paste URLs of albums to download here.\nA Bandcamp URL looks like: http://[artist name].bandcamp.com/album/[album name]\nYou can also paste track pages: http://[artist name].bandcamp.com/track/[track name]\nOr artist pages: http://[artist name].bandcamp.com and check \"☑ Force download of all albums\" to download the whole artist's discography.\nYou can specify multiple URLs by writing one URL per line.";
         /// <summary>
         /// The absolute path to the settings file.
         /// </summary>
