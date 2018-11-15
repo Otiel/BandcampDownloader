bf1569ed8f4ccfaefaadf64c71021c1759e0e20e
Otiel <web@otiel.fr>
2018-11-15 18:58:39 +0100

    fix: fix exception that would occur if AllowableFileSizeDifference textbox was emptied

---
+++ Sources/BandcampDownloader/Miscellaneous/DoubleRule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace BandcampDownloader {
+
+    public class DoubleRule: ValidationRule {
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo) {
+            if (Double.TryParse(value.ToString(), NumberStyles.Float, CultureInfo.InvariantCulture, out Double doubleValue)) {
+                return new ValidationResult(true, null);
+            } else {
+                return new ValidationResult(false, "Not a numeric value");
+            }
+        }
+    }
+}
