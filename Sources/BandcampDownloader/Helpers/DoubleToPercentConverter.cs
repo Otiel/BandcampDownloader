25ac1ec1ea6fde0c282799816c5936d14727db38
Otiel <web@otiel.fr>
2018-11-15 12:24:21 +0100

    refactor: remove useless code

--- Sources/BandcampDownloader/Helpers/DoubleToPercentConverter.cs
+++ Sources/BandcampDownloader/Helpers/DoubleToPercentConverter.cs
@@ -10,20 +10,11 @@ namespace BandcampDownloader {
     public class DoubleToPercentConverter: IValueConverter {
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-            if (!(value is Double percentValue)) {
-                // Shouldn't happen
-                throw new ArgumentException("Value should be a Double", nameof(value));
-            }
-            return percentValue * 100; // 0.05 → 5%
+            return Double.Parse(value.ToString(), NumberStyles.Any, CultureInfo.InvariantCulture) * 100; // 0.05 → 5%
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
-            if (!Double.TryParse(value.ToString(), NumberStyles.Any, CultureInfo.InvariantCulture, out Double doubleValue)) {
-                // Shouldn't happen
-                throw new ArgumentException("Value should be a Double", nameof(value));
-            }
-
-            return doubleValue / 100; // 5% → 0.05
+            return Double.Parse(value.ToString(), NumberStyles.Any, CultureInfo.InvariantCulture) / 100; // 5% → 0.05
         }
     }
 }
