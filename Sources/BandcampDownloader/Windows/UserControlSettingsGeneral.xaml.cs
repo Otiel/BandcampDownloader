ab3a9bc46df83c982eacc505047630ae25cde70a
Otiel <Otiel@users.noreply.github.com>
2018-12-01 18:27:49 +0100

    feat: add button to check for updates

--- Sources/BandcampDownloader/Windows/UserControlSettingsGeneral.xaml.cs
+++ Sources/BandcampDownloader/Windows/UserControlSettingsGeneral.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace BandcampDownloader {
 
@@ -25,6 +29,31 @@ namespace BandcampDownloader {
         public void SaveSettings() {
             checkBoxCheckForUpdates.GetBindingExpression(CheckBox.IsCheckedProperty).UpdateSource();
             checkBoxVerboseLog.GetBindingExpression(CheckBox.IsCheckedProperty).UpdateSource();
+        }
+
+        private void ButtonCheckForUpdates_Click(object sender, RoutedEventArgs e) {
+            Version latestVersion = null;
+            try {
+                latestVersion = UpdatesHelper.GetLatestVersion();
+            } catch (CouldNotCheckForUpdatesException) {
+                MessageBox.Show("An error occured while checking for updates. Please retry later.", "Bandcamp Downloader", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            Version currentVersion = Assembly.GetExecutingAssembly().GetName().Version;
+            if (currentVersion.CompareTo(latestVersion) < 0) {
+                // The latest version is newer than the current one
+                String msgBoxText =
+                    "A new version is available:" + Environment.NewLine +
+                    "Current version = " + currentVersion + Environment.NewLine +
+                    "Latest version = " + latestVersion + Environment.NewLine + Environment.NewLine +
+                    "Would you like to go to the project website in order to download the latest version?";
+                if (MessageBox.Show(msgBoxText, "Bandcamp Downloader", MessageBoxButton.YesNo, MessageBoxImage.Information, MessageBoxResult.Yes) == MessageBoxResult.Yes) {
+                    Process.Start(Constants.ProjectWebsite);
+                }
+            } else {
+                MessageBox.Show("You already have the latest version available (" + currentVersion + ").", "Bandcamp Downloader", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
         }
     }
 }
