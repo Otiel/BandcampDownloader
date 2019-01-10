8f0679163c41c61c2859657c0af58b697550e4fe
Otiel <Otiel@users.noreply.github.com>
2019-01-10 14:11:17 +0100

    refactor: remove unused "using"

--- src/BandcampDownloader/UI/Dialogs/WindowMain.xaml.cs
+++ src/BandcampDownloader/UI/Dialogs/WindowMain.xaml.cs
@@ -10,7 +10,6 @@ using System.Net;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
