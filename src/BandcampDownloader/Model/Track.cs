d8e12f377590ab9d5875eb290330134bf22a554e
Otiel <Otiel@users.noreply.github.com>
2019-04-24 14:57:29 +0200

    feat: retrieve tracks duration from Json

--- src/BandcampDownloader/Model/Track.cs
+++ src/BandcampDownloader/Model/Track.cs
@@ -3,6 +3,7 @@
 namespace BandcampDownloader {
 
     internal class Track {
+        public Double Duration { get; set; } // In seconds
         public String Lyrics { get; set; }
         public String Mp3Url { get; set; }
         public int Number { get; set; }
