3b1dcb2685d53e4c88fe518a64205a42f74d6ac7
Otiel <Otiel@users.noreply.github.com>
2019-04-25 16:30:12 +0200

    refactor: add XML doc on properties

--- src/BandcampDownloader/Model/Album.cs
+++ src/BandcampDownloader/Model/Album.cs
@@ -4,17 +4,39 @@ using System.Collections.Generic;
 namespace BandcampDownloader {
 
     internal class Album {
+
+        /// <summary>
+        /// The album artist.
+        /// </summary>
         public String Artist { get; set; }
+
+        /// <summary>
+        /// The URL where the artwork should be downloaded from.
+        /// </summary>
         public String ArtworkUrl { get; set; }
 
+        /// <summary>
+        /// True if the album has an artwork; false otherwise.
+        /// </summary>
         public Boolean HasArtwork {
             get {
                 return ArtworkUrl != null;
             }
         }
 
+        /// <summary>
+        /// The release date of the album.
+        /// </summary>
         public DateTime ReleaseDate { get; set; }
+
+        /// <summary>
+        /// The album title.
+        /// </summary>
         public String Title { get; set; }
+
+        /// <summary>
+        /// The list of tracks contained in the album.
+        /// </summary>
         public List<Track> Tracks { get; set; }
     }
 }
--- src/BandcampDownloader/Model/Track.cs
+++ src/BandcampDownloader/Model/Track.cs
@@ -3,10 +3,30 @@
 namespace BandcampDownloader {
 
     internal class Track {
-        public Double Duration { get; set; } // In seconds
+
+        /// <summary>
+        /// The track length (in seconds).
+        /// </summary>
+        public Double Duration { get; set; }
+
+        /// <summary>
+        /// The track lyrics.
+        /// </summary>
         public String Lyrics { get; set; }
+
+        /// <summary>
+        /// The URL where the track should be downloaded from.
+        /// </summary>
         public String Mp3Url { get; set; }
+
+        /// <summary>
+        /// The track number.
+        /// </summary>
         public int Number { get; set; }
+
+        /// <summary>
+        /// The track title.
+        /// </summary>
         public String Title { get; set; }
     }
 }
