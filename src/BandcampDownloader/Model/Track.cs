3d0ba0b2b1fd6a8e71b27c1c8a6f588c52d3399e
Otiel <Otiel@users.noreply.github.com>
2019-04-25 18:43:27 +0200

    refactor: SetPath method

--- src/BandcampDownloader/Model/Track.cs
+++ src/BandcampDownloader/Model/Track.cs
@@ -50,7 +50,7 @@ namespace BandcampDownloader {
             Number = number;
             Title = title;
             // Must be done after other properties are filled!
-            SetPath();
+            Path = ParseTrackFilePath();
         }
 
         /// <summary>
@@ -70,18 +70,21 @@ namespace BandcampDownloader {
         }
 
         /// <summary>
-        /// Sets the Path property of the current Track.
+        /// Returns the file path to be used for the track from the file name format saved in the UserSettings, by
+        /// replacing the placeholders strings with their corresponding values. The returned file path DOES contain the extension.
         /// </summary>
-        private void SetPath() {
+        private String ParseTrackFilePath() {
             String fileName = ParseTrackFileName();
 
-            Path = Album.Path + "\\" + fileName;
-            if (Path.Length >= 260) {
+            String path = Album.Path + "\\" + fileName;
+            if (path.Length >= 260) {
                 // Windows doesn't do well with path + filename >= 260 characters (and path >= 248 characters)
                 // album.Path has been shorten to 247 characters before, so we have 12 characters max left for filename.ext
-                int fileNameMaxLength = 12 - System.IO.Path.GetExtension(Path).Length;
-                Path = Album.Path + "\\" + fileName.Substring(0, fileNameMaxLength) + System.IO.Path.GetExtension(Path);
+                int fileNameMaxLength = 12 - System.IO.Path.GetExtension(path).Length;
+                path = Album.Path + "\\" + fileName.Substring(0, fileNameMaxLength) + System.IO.Path.GetExtension(path);
             }
+
+            return path;
         }
     }
 }
