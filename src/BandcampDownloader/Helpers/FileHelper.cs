2ba0310fe6011d3f4d7714c52269019822b7f97e
Otiel <Otiel@users.noreply.github.com>
2019-04-26 14:35:44 +0200

    refactor: use explicit type instead of var

--- src/BandcampDownloader/Helpers/FileHelper.cs
+++ src/BandcampDownloader/Helpers/FileHelper.cs
@@ -14,7 +14,7 @@ namespace BandcampDownloader {
         /// size.</param>
         /// <returns>The size of the file located at the provided URL.</returns>
         public static async Task<long> GetFileSizeAsync(String url, String protocolMethod) {
-            var webRequest = HttpWebRequest.Create(url);
+            WebRequest webRequest = HttpWebRequest.Create(url);
             webRequest.Method = protocolMethod;
             long fileSize;
             try {
