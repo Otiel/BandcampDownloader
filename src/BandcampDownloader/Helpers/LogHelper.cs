826485d269e28067edb68578307886aeb2b58b66
Otiel <Otiel@users.noreply.github.com>
2019-05-30 11:34:54 +0200

    feat: set log colors for dark theme

--- src/BandcampDownloader/Helpers/LogHelper.cs
+++ src/BandcampDownloader/Helpers/LogHelper.cs
@@ -14,22 +14,22 @@ namespace BandcampDownloader {
 
             switch (logType) {
                 case LogType.Info:
-                    color = Brushes.Black;
+                    color = App.UserSettings.Theme == Skin.Dark ? new SolidColorBrush((Color) ColorConverter.ConvertFromString("#f0f0f0")) : Brushes.Black;
                     break;
                 case LogType.VerboseInfo:
-                    color = Brushes.MediumBlue;
+                    color = App.UserSettings.Theme == Skin.Dark ? Brushes.LightSkyBlue : Brushes.MediumBlue;
                     break;
                 case LogType.IntermediateSuccess:
-                    color = Brushes.MediumBlue;
+                    color = App.UserSettings.Theme == Skin.Dark ? Brushes.LightSkyBlue : Brushes.MediumBlue;
                     break;
                 case LogType.Success:
-                    color = Brushes.Green;
+                    color = App.UserSettings.Theme == Skin.Dark ? Brushes.Lime : Brushes.Green;
                     break;
                 case LogType.Warning:
-                    color = Brushes.OrangeRed;
+                    color = App.UserSettings.Theme == Skin.Dark ? Brushes.Orange : Brushes.OrangeRed;
                     break;
                 case LogType.Error:
-                    color = Brushes.Red;
+                    color = App.UserSettings.Theme == Skin.Dark ? Brushes.Red : Brushes.Red;
                     break;
                 default:
                     throw new NotImplementedException();
