860c10b8d886780c881072a6b6e83b4d4126c51c
Otiel <Otiel@users.noreply.github.com>
2019-06-13 17:08:07 +0200

    docs: fix XML comments formatting

--- src/BandcampDownloader/Helpers/FileHelper.cs
+++ src/BandcampDownloader/Helpers/FileHelper.cs
@@ -28,8 +28,7 @@ namespace BandcampDownloader {
         /// Returns the size of the file located at the provided URL.
         /// </summary>
         /// <param name="url">The URL.</param>
-        /// <param name="protocolMethod">The protocol method to use in order to retrieve the file
-        /// size.</param>
+        /// <param name="protocolMethod">The protocol method to use in order to retrieve the file size.</param>
         /// <returns>The size of the file located at the provided URL.</returns>
         public static async Task<long> GetFileSizeAsync(string url, string protocolMethod) {
             WebRequest webRequest = HttpWebRequest.Create(url);
