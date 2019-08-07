Otiel/BandcampDownloader

commit 25d339ead7a32d2175e000fcb7ed45e708f90157

fix: apply language only when necessary

src/BandcampDownloader/UI/Dialogs/Settings/UserControlSettingsGeneral.xaml.cs
@@ -19,8 +19,11 @@ namespace BandcampDownloader {
         /// Cancels the changes already applied.
         /// </summary>
         public void CancelChanges() {
-            LanguageHelper.ApplyLanguage(App.UserSettings.Language);
             ThemeHelper.ApplySkin(App.UserSettings.Theme);
+            // Revert the language only if it has been changed
+            if ((Language) comboBoxLanguage.SelectedValue != App.UserSettings.Language) {
+                LanguageHelper.ApplyLanguage(App.UserSettings.Language);
+            }
         }
 
         /// <summary>
@@ -81,8 +84,10 @@ namespace BandcampDownloader {
         }
 
         private void ComboBoxLanguage_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            // Apply selected language
-            LanguageHelper.ApplyLanguage((Language) comboBoxLanguage.SelectedValue);
+            if ((Language) comboBoxLanguage.SelectedValue != App.UserSettings.Language) {
+                // Apply selected language
+                LanguageHelper.ApplyLanguage((Language) comboBoxLanguage.SelectedValue);
+            }
         }
 
         private void ComboBoxTheme_SelectionChanged(object sender, SelectionChangedEventArgs e) {

commit 04014935f531e710d026ddb2cbf0384637313edc

fix: language not changed when cycling back
Fixes bug introduced in 25d339e

src/BandcampDownloader/UI/Dialogs/Settings/UserControlSettingsGeneral.xaml.cs
@@ -87,10 +87,12 @@ namespace BandcampDownloader {
         }
 
         private void ComboBoxLanguage_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            if ((Language) comboBoxLanguage.SelectedValue != App.UserSettings.Language) {
-                // Apply selected language
-                LanguageHelper.ApplyLanguage((Language) comboBoxLanguage.SelectedValue);
+            if (!comboBoxLanguage.IsLoaded) {
+                return;
             }
+
+            // Apply selected language
+            LanguageHelper.ApplyLanguage((Language) comboBoxLanguage.SelectedValue);
         }
 
         private void ComboBoxTheme_SelectionChanged(object sender, SelectionChangedEventArgs e) {