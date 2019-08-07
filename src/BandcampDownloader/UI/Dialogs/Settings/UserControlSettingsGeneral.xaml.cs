Otiel/BandcampDownloader

commit f67b1cf2af0944b8c3907fabe18f210a506bbff1

fix: apply theme only when necessary
Fixes #112

src/BandcampDownloader/UI/Dialogs/Settings/UserControlSettingsGeneral.xaml.cs
@@ -19,10 +19,13 @@ namespace BandcampDownloader {
         /// Cancels the changes already applied.
         /// </summary>
         public void CancelChanges() {
-            ThemeHelper.ApplySkin(App.UserSettings.Theme);
             // Revert the language only if it has been changed
             if ((Language) comboBoxLanguage.SelectedValue != App.UserSettings.Language) {
                 LanguageHelper.ApplyLanguage(App.UserSettings.Language);
+            }
+            // Revert the skin only if it has been changed
+            if ((Skin) comboBoxTheme.SelectedItem != App.UserSettings.Theme) {
+                ThemeHelper.ApplySkin(App.UserSettings.Theme);
             }
         }
 
@@ -91,8 +94,10 @@ namespace BandcampDownloader {
         }
 
         private void ComboBoxTheme_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            // Apply selected theme
-            ThemeHelper.ApplySkin((Skin) comboBoxTheme.SelectedItem);
+            if ((Skin) comboBoxTheme.SelectedItem != App.UserSettings.Theme) {
+                // Apply selected theme
+                ThemeHelper.ApplySkin((Skin) comboBoxTheme.SelectedItem);
+            }
         }
 
         private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e) {

commit 04fc8b5b2e0c63d6713105a13101caac54360ed6

fix: theme not changed when cycling back
Correctly fixes #112, fixes bug introduced in f67b1cf

src/BandcampDownloader/UI/Dialogs/Settings/UserControlSettingsGeneral.xaml.cs
@@ -96,10 +96,12 @@ namespace BandcampDownloader {
         }
 
         private void ComboBoxTheme_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            if ((Skin) comboBoxTheme.SelectedItem != App.UserSettings.Theme) {
-                // Apply selected theme
-                ThemeHelper.ApplySkin((Skin) comboBoxTheme.SelectedItem);
+            if (!comboBoxTheme.IsLoaded) {
+                return;
             }
+
+            // Apply selected theme
+            ThemeHelper.ApplySkin((Skin) comboBoxTheme.SelectedItem);
         }
 
         private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e) {