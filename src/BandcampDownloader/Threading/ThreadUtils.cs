ff0835b8645e481e39f69dbba18195497add6e6c
Otiel <Otiel@users.noreply.github.com>
2025-11-09 20:39:59 +0100

    Fix most UI freezes when downloading

---
+++ src/BandcampDownloader/Threading/ThreadUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BandcampDownloader.Threading;
+
+internal static class ThreadUtils
+{
+    public static async Task ExecuteOnUiAsync(Action action)
+    {
+        if (Application.Current.Dispatcher.CheckAccess())
+        {
+            // Already on UI thread
+            action();
+        }
+        else
+        {
+            await Application.Current.Dispatcher.InvokeAsync(action);
+        }
+    }
+
+    public static async Task<T> ExecuteOnUiAsync<T>(Func<T> action)
+    {
+        T result;
+        if (Application.Current.Dispatcher.CheckAccess())
+        {
+            // Already on UI thread
+            result = action();
+        }
+        else
+        {
+            result = await Application.Current.Dispatcher.InvokeAsync(action);
+        }
+
+        return result;
+    }
+}
