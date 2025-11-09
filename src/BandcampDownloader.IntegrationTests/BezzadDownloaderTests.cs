e5e04214129c88bd23106dd9ad2def41f6580d28
Otiel <Otiel@users.noreply.github.com>
2025-11-10 00:08:19 +0100

    Add IntegrationTests

---
+++ src/BandcampDownloader.IntegrationTests/BezzadDownloaderTests.cs
@@ -0,0 +1,36 @@
+﻿using Downloader;
+
+namespace BandcampDownloader.IntegrationTests;
+
+public sealed class BezzadDownloaderTests
+{
+    private DownloadService _sut;
+
+    [SetUp]
+    public void Setup()
+    {
+        _sut = new DownloadService();
+    }
+
+    [Test]
+    [Ignore("Test fails")]
+    public void RequestingCancellation_Throw_OperationCanceledException()
+    {
+        // Arrange
+        const string fileUrl = "https://www.examplefile.com/file-download/202"; // 1 GB txt file
+        var cts = new CancellationTokenSource();
+        cts.CancelAfter(500);
+
+        Assert.ThrowsAsync<OperationCanceledException>(async () =>
+        {
+            // Act
+            await _sut.DownloadFileTaskAsync(fileUrl, cts.Token);
+        });
+    }
+
+    [TearDown]
+    public void Cleanup()
+    {
+        _sut.Dispose();
+    }
+}
