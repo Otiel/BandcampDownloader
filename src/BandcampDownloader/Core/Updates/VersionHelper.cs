a0df01b962ee0e73d74aca15209fbcfcdca3a38e
Otiel <Otiel@users.noreply.github.com>
2025-11-30 13:41:32 +0100

    Remove GetCurrentVersion, use Constants.AppVersion instead

--- src/BandcampDownloader/Core/Updates/VersionHelper.cs
+++ src/BandcampDownloader/Core/Updates/VersionHelper.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Reflection;
 
 namespace BandcampDownloader.Core.Updates;
 
 internal static class VersionHelper
 {
-    public static Version GetCurrentVersion()
-    {
-        return Assembly.GetExecutingAssembly().GetName().Version;
-    }
-
     public static bool IsNewerVersion(this Version version)
     {
-        var currentVersion = Assembly.GetExecutingAssembly().GetName().Version;
-        return currentVersion!.CompareTo(version) < 0;
+        return Constants.AppVersion.CompareTo(version) < 0;
     }
 }
