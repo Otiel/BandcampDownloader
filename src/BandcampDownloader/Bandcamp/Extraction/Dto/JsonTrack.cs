6d30cc2f28c00d404bb5330b41515451f1237bd7
Otiel <Otiel@users.noreply.github.com>
2025-11-30 17:17:12 +0100

    Fix string comparison

--- src/BandcampDownloader/Bandcamp/Extraction/Dto/JsonTrack.cs
+++ src/BandcampDownloader/Bandcamp/Extraction/Dto/JsonTrack.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System;
+using System.Text.Json.Serialization;
 using BandcampDownloader.Model;
 
 namespace BandcampDownloader.Bandcamp.Extraction.Dto;
@@ -22,7 +23,7 @@ internal sealed class JsonTrack
 
     public Track ToTrack(Album album)
     {
-        var mp3Url = (File.Url.StartsWith("//") ? "http:" : "") + File.Url; // "//example.com" Uri lacks protocol
+        var mp3Url = (File.Url.StartsWith("//", StringComparison.Ordinal) ? "http:" : "") + File.Url; // "//example.com" Uri lacks protocol
         var number = Number == 0 ? 1 : Number; // For bandcamp track pages, Number will be 0. Set 1 instead
 
         return new Track(album, Duration, Lyrics, mp3Url, number, Title);
